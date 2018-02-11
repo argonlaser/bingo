9a5fe6ddc7aad07f2652ec39a2df770aa7df217b
argonlaser <vkvenkat94@gmail.com>
2018-02-11 23:01:56 +0530

    removes unused code as shuffling handled at client side.
    Signed-off-by: argonlaser <vkvenkat94@gmail.com>

--- gameShip/game/BoardBox.go
+++ gameShip/game/BoardBox.go
@@ -1,7 +1,5 @@
 package game
 
-import "math/rand"
-
 // BoardBox instance
 type BoardBox struct {
 	IsStriked bool
@@ -10,21 +8,3 @@ type BoardBox struct {
 	Val       int32
 }
 
-// fillBoardBox gets player in a game by ID
-func (b *BoardBox) fillBoardBox() (*BoardBox, error) {
-	list := rand.Perm(25)
-	for i, _ := range list {
-		list[i]++
-	}
-
-	var i = 0
-	for rowVal := 1; rowVal <= 5; rowVal++ {
-		for colVal := 1; colVal <= 5; colVal++ {
-			b.IsStriked = false
-			b.Row = int32(rowVal)
-			b.Col = int32(colVal)
-			b.Val = int32(list[i-1])
-			i++
-		}
-	}
-}
